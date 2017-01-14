2c1aa4727f0d5d6a3475793e5933e40152ec63e2
Emily Mendez <emilypmendez@gmail.com>
2017-01-14 17:09:01 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+// main.js
+// 
+// date = current date
+// input = number input
+// day = "days" selected
+// week = "weeks" selected
+// remind = final reminder date
+
+function setDate (date, input) {
+	var date = document.getElementById('currentdate');
+	var input = document.getElementById('userinput');
+	var day = document.getElementById('selectday');
+	var week = document.getElementById('selectweek');
+	var remind = document.getElementById('reminderdate');
+
+	for (every time user selects date range) {
+		if (user selects between day / week) {
+			return true;
+		else {
+			return false;
+		}
+		}
+	}
+}
